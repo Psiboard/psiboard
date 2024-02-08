1dafd9f78eaa2d24db685ee0bf63000cc7b2942b
NogueiraDan <silvanogueira3460@gmail.com>
2024-02-08 08:41:51 -0300

    feat: Estiliza modal de edição

--- src/components/modal-edit.tsx
+++ src/components/modal-edit.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "@chakra-ui/react";
+
 export default function ModalEdit({isOpen, onClose}:any) {
   const schedules = [
     "07:00",
@@ -19,23 +21,46 @@ export default function ModalEdit({isOpen, onClose}:any) {
   ];
   if(isOpen){
      return (
-       <div>
-         <div>
-           <div>
+       <div className="fixed inset-0 z-[9] bg-[rgb(0,0,0,0.3)] ">
+         <div className="fixed -translate-x-2/4 -translate-y-2/4 w-[450px] bg-[#fff] rounded-2xl left-2/4 top-2/4">
+           <div className="bg-[#02969c] text-[#fff] flex justify-between p-8 rounded-[16px_16px_0_0]">
              <h2>Editar Horário</h2>
-             <button>Editar</button>
+             <button onClick={onClose}>
+               <svg
+                 xmlns="http://www.w3.org/2000/svg"
+                 fill="none"
+                 viewBox="0 0 24 24"
+                 strokeWidth={1.5}
+                 stroke="currentColor"
+                 className="w-6 h-6"
+               >
+                 <path
+                   strokeLinecap="round"
+                   strokeLinejoin="round"
+                   d="M6 18 18 6M6 6l12 12"
+                 />
+               </svg>
+             </button>
            </div>
-           <div>
-             <p>Daniel - 07:00h</p>
 
-             <div>
+           <div className="px-8 py-4">
+             <p className="text-2xl mb-[0.8rem];">Daniel - 07:00h</p>
+
+             <div className="flex justify-between items-center mb-6">
                <label htmlFor="">Indique uma nova data</label>
-               <input type="date" />
+               <input
+                 type="date"
+                 className="border w-2/5 p-[0.3rem] rounded-[10px] border-solid"
+               />
              </div>
 
-             <div>
+             <div className="flex justify-between items-center mb-6">
                <label htmlFor="">Horários disponiveis</label>
-               <select name="" id="">
+               <select
+                 name=""
+                 id=""
+                 className="border w-2/5 p-[0.3rem] rounded-[10px] border-solid"
+               >
                  {schedules
                    ? schedules.map((hora: any, index: any) => {
                        return (
@@ -48,7 +73,7 @@ export default function ModalEdit({isOpen, onClose}:any) {
                </select>
              </div>
 
-             <div className="">
+             <div className="flex justify-between items-center mb-6">
                <label htmlFor="">Marque o campo de remarcação</label>
                <input
                  type="checkbox"
@@ -58,9 +83,13 @@ export default function ModalEdit({isOpen, onClose}:any) {
                />
              </div>
            </div>
-           <div>
-             <button onClick={onClose}>Cancelar</button>
-             <button>Editar</button>
+           <div className=" flex justify-between pt-0 pb-8 px-8">
+             <Button colorScheme="red" size="sm" onClick={onClose}>
+               Cancelar
+             </Button>
+             <Button colorScheme="teal" size="sm">
+               Editar
+             </Button>
            </div>
          </div>
        </div>
