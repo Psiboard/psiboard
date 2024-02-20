b7ed65a9457112b814c49b5847ab12e7b2b05763
NogueiraDan <silvanogueira3460@gmail.com>
2024-02-20 20:58:46 -0300

    feat: Setup inicial React Query

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import { RouteApp } from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./contexts/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <ToastContainer />
-      <AuthProvider>
-        <RouteApp />
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ToastContainer />
+        <AuthProvider>
+          <RouteApp />
+        </AuthProvider>
+        <ReactQueryDevtools buttonPosition="bottom-right" position="right" />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
