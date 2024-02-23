de7843b640e347c313695de106bfd5b00fb3f50a
NogueiraDan <silvanogueira3460@gmail.com>
2024-02-22 22:13:43 -0300

    feat: Hook de reagendamento

---
+++ src/hooks/useUpdateSchedule.ts
@@ -0,0 +1,29 @@
+import { useMutation } from "@tanstack/react-query";
+import api from "../services/api";
+import { toast } from "react-toastify";
+import { isAxiosError } from "axios";
+
+export function useUpdatePatient() {
+  const {mutateAsync} = useMutation({
+    mutationFn: async ({ body, scheduleId }: any): Promise<any> => {
+      const response = await api.patch(`/scheduling/${scheduleId}`, body);
+      return response.data;
+    },
+    onSuccess: () => {
+      toast.success("Agendamento atualizado com sucesso!");
+      window.location.reload();
+    },
+    onError(error) {
+      console.log("Caiu no onError do useCreateSchedule", error);
+      if (isAxiosError(error)) {
+        toast.error(error.response?.data.message);
+        console.log(error);
+      }
+    },
+  });
+
+   return {
+     updatePatient: mutateAsync,
+   };
+
+}
