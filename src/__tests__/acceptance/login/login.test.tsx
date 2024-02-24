4a03633441b065d4354b611db127c88ef391416f
NogueiraDan <silvanogueira3460@gmail.com>
2024-02-24 10:29:15 -0300

    feat: Teste de integração Login-Dashboard

--- src/__tests__/acceptance/login/login.test.tsx
+++ src/__tests__/acceptance/login/login.test.tsx
@@ -1,13 +1,23 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { expect, describe, it } from "vitest";
-import { Login,Dashboard } from "../../../pages";
+import { expect, describe, it, vi } from "vitest";
+import { Login, Dashboard } from "../../../pages";
 import { AuthProvider } from "../../../contexts/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+
+// Mock do useAuth
+ const signInMock = vi.fn();
+ vi.mock("../../../hooks/useAuth", () => ({
+   useAuth: () => ({
+     signIn: signInMock,
+     user: { id: "user123" },
+   }),
+ }));
+
 describe("Feature Login", () => {
-  it("should login with crendentials valids", async () => {
+  it("should be integrate Login Component with Dashboard Component", async () => {
     const queryClient = new QueryClient();
     render(
       <QueryClientProvider client={queryClient}>
@@ -22,7 +32,7 @@ describe("Feature Login", () => {
 
     // Preenchendo o campo de email
     const emailInput = screen.getByLabelText("Email");
-    await userEvent.type(emailInput, "daniel@teste.com");
+    await userEvent.type(emailInput, "teste@teste.com");
 
     // Preenchendo o campo de senha
     const passwordInput = screen.getByLabelText("Password");
@@ -31,9 +41,12 @@ describe("Feature Login", () => {
     // Submetendo o formulário
     await userEvent.click(screen.getByRole("button", { name: /Entrar/i }));
 
-
     await waitFor(() => {
       expect(screen.getByText(/Bem vindo/i)).toBeInTheDocument();
+       expect(signInMock).toHaveBeenCalledWith({
+         email: "teste@teste.com",
+         password: "teste",
+       });
     });
   });
 });
