8c0d5c8b2aad3de26a5bfe035727f3ffdf9dc784
Daniel Nogueira <silvanogueira3460@gmail.com>
2024-06-14 20:59:13 -0300

    feat: Refatora Contexto. Context -> Zustand

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -11,7 +11,7 @@ describe("Feature Login", () => {
     cy.get("#email").type("usuario.demo@teste.com");
     cy.get("#password").type("testedemo");
     cy.get('button[type="submit"]').click();
-    cy.wait(8000);
+    cy.wait(6000);
     cy.get('[data-testid="button-mobile-menu"]').click();
     cy.get('[data-testid="button-logout"]').click();
   });
---
+++ src/stores/authStore.ts
@@ -0,0 +1,54 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import Cookies from "js-cookie";
+import api from "../services/api";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+type ISignIn = {
+  email: string;
+  password: string;
+};
+
+type State = {
+  user: {
+    nome: string;
+    email: string;
+    id: number;
+  } | null;
+  isAuthenticated: boolean;
+  loading: boolean;
+  signIn: ({ email, password }: ISignIn) => Promise<void>;
+  signOut: () => void;
+};
+
+export const useAuthStore = create<State>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  loading: false,
+  signIn: async ({ email, password }) => {
+    try {
+      set({ loading: true });
+      const { data } = await api.post("/auth/login", { email, password });
+      const userData = {
+        email: data.email,
+        nome: data.name,
+        id: data.id,
+      };
+      Cookies.set("user@data", JSON.stringify(userData), {
+        expires: 1 / 24,
+      });
+      Cookies.set("token@data", data.access_token, { expires: 1 / 24 });
+      set({ user: userData, isAuthenticated: true, loading: false });
+      toast.success(`Seja bem vindo(a), ${userData.nome}`);
+    } catch {
+      set({ loading: false });
+      toast.error("Não conseguimos realizar o login. Tente mais tarde");
+    }
+  },
+  signOut: () => {
+    Cookies.remove("user@data");
+    Cookies.remove("token@data");
+    set({ user: null, isAuthenticated: false });
+  },
+}));
